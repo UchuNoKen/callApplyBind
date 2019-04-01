2bfb161fa55369f8b983c56149ca7929cc51a853
UchuNoKen <xen.o.fite@gmail.com>
2019-04-01 18:58:55 -0400

    function prototype methods

---
+++ applyCallBind.js
@@ -0,0 +1,10 @@
+// Function.prototype methods
+
+// Function.prototype.apply()
+// Function.prototype.bind()
+// Function.prototype.call()
+// Function.prototype.isGenerator()
+// Function.prototype.toSource()
+// Object.prototype.toSource
+// Function.prototype.toString()
+// Object.prototype.toString
